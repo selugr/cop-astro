cef3d2bd7b05ab7d6ff0946c3ffa19421284c229
jorgevillalta <j_villalta@hotmail.com>
2022-10-21 09:13:18 +0200

    Add and remove dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
-
+import image from '@astrojs/image'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), image()],
 })
---
+++ src/components/Card.jsx
@@ -0,0 +1,3 @@
+export default function Card({ image }) {
+  return <div>{image}</div>
+}
